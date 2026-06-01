923aedc9c782134a2b40ca3fc3af9c4cffa7e9c6
Hili <sy1253079@gmail.com>
2026-06-02 01:27:26 +0300

    part 1

--- 11-object-and-arrays/01-part-1.js
+++ 11-object-and-arrays/01-part-1.js
@@ -22,3 +22,12 @@ console.log(city);
 console.log(first);
 
 //1.2
+const growScores = [88, 92, 100]
+
+const maxScores = Math.max(...growScores)
+
+const otherGrades = growScores.filter(score => score !== maxScores)
+
+console.log(`Grow scores is: ${maxScores}, and the rest is: ${otherGrades}`);
+
+console.log(otherGrades);
