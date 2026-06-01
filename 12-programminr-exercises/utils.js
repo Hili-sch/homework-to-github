258a0394d434508f2f3e32096a60aa2b7f413405
Hili <sy1253079@gmail.com>
2026-06-02 01:05:46 +0300

    first commit homeork 12

--- 12-programminr-exercises/utils.js
+++ 12-programminr-exercises/utils.js
@@ -2,7 +2,7 @@ export function getInteger(min = 1000, top = 9999) {
     return Math.floor(Math.random() * (top - min) + min)
 }
 
-export function getInteger1(min = 1000, top = 9999) {
+export function getNumber(min = 1000, top = 9999) {
     return Math.random() * (top - min) + min
 }
 
